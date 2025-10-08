e4a68210ce3dbef84199cd8574f6e5e71160dde1
unknown <185297@bme.hu>
2025-10-08 14:59:21 +0200

    FirstError

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,9 +28,10 @@ public class TorpedoStore {
     }
   }
 
+  
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
